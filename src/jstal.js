8d7e1c62d73bff56d32190fb8cc9611c992eef54
Sönke Sprink <soenke@six.de>
2013-01-24 10:06:23 +0100

    lets do it the vanilla way

--- src/jstal.js
+++ src/jstal.js
@@ -5,19 +5,32 @@
  * Time: 20:50
  * To change this template use File | Settings | File Templates.
  */
-
+"use strict";
+var cont;
 var JstalEngine = function(tal){
     this.config = {
-      talAttr: "tal:"
+        talAttr: "tal:",
+        talSelector: "tal\\3a "
     };
-    this.template = $(tal._template);
-    this.data = tal;
+    this._retrieveData = function(data){
+        this.data = data;
+    };
+    this._setTemplate = function(string){
+        var wrapper= document.createElement('div');
+        wrapper.innerHTML = string;
+        this.template = wrapper.firstChild;
+        cont = this.template;
+    };
     this.define = function(){}
+    this._selector = function(context, query){
+        return context.querySelectorAll("[tal\\3a " + query + "]");
+
+    };
     this.condition = function(){}
     this.repeat = function(){}
     this.content = function(node){
-        var value = node.attr("tal:content").split(/ /);
-        node[("structure" == value[0] ? "html" : "text")](this._getData(value[value.length-1]));
+        var value = node.getAttribute("tal:content").split(/ /);
+        node[("structure" == value[0] ? "innerHTML" : "innerText")] = this._getData(value[value.length-1]);
     };
     this._getData = function(raw){
         var data = raw.split(/\//),
@@ -32,24 +45,33 @@ var JstalEngine = function(tal){
         return piece;
     };
     this._collect = function() {
-        var self = this;
         for (var func in this){
             if ("function"==typeof(this[func]) && "_"!=func.substr(0,1)){
-                this.template.find('[tal\\:' + func + ']').each(function(){
-                    self[func]($(this));
-                });
+                var elements = this._selector(this.template, func);
+                for (var i = 0; i<elements.length; i++) {
+                    this[func](elements[i]);
+                }
             }
         }
-    }
+    };
     this._execute = function(){
         this._collect();
-
-
-
         return this.template;
-    }
+    };
+    this._setTemplate(tal._template);
+    this._retrieveData(tal);
 };
 
+if (window.jQuery1) {
+    JstalEngine.prototype = {
+        _selector: function(context, query){
+            return $(context).find(query);
+        },
+        _setTemplate: function(string){
+            return $(string);
+        }
+    }
+}
 
 var Jstal = function(template) {
     this._template = template;
